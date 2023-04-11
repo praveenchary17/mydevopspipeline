4b9b8897e018408f6ddba72e6239e43940d05093
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-04-11 21:29:43 +0530

    Update ec2.tf

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -2,11 +2,8 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-
   vpc_security_group_ids = var.vpc_security_group_ids
-
   key_name = var.key_name
-
   tags = {
     Name = var.name
     app = "myapp"
