728a79ad1d2ec6bbc586aee4ffb55a59d82bf063
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-04-11 20:51:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 module "ec2_instance" {
   source = "./ec2-instance"
 
@@ -15,12 +7,4 @@ module "ec2_instance" {
   subnet_id            = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   key_name             = var.key_name
-}
-
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
 }
