81c5fe6c59158fa7732fcde0a7a273d4c6f2fa2c
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-04-11 21:54:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-module "ec2_instance" {
-  source = "modules/"
-
-  name                 = "my-ec2-instance"
-  ami                  = var.ami_id
-  instance_type        = var.instance_type
-  subnet_id            = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-  key_name             = var.key_name
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-06e46074ae430fba6"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-064b017ba64e7a4ff"
+  vpc_security_group_ids = "sg-08bcea0cf4ebeea3a"
+  key_name = "mydevopspipeline"
+  tags = {
+    Name = "myec2instance"
+    app = "myapp"
+  }
 }
